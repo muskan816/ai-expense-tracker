50a136fb09766780a69fc7474df1525a2db61ea1
Muskan <muskmane420@gmail.com>
2026-07-15 00:01:36 +0530

    mobile-bar and sidebar

--- frontend/src/components/layout/MobileMenu.jsx
+++ frontend/src/components/layout/MobileMenu.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { MdDashboard } from "react-icons/md";
+import { BsStack } from "react-icons/bs";
+import { HiDocumentChartBar } from "react-icons/hi";
+import {
+  FaChartBar,
+  FaCog,
+  FaMoneyBillWave,
+  FaRobot,
+  FaWallet,
+  FaTimes,
+} from "react-icons/fa";
+import { NavLink, useNavigate } from "react-router";
 
-const MobileMenu = () => {
+const MobileMenu = ({ isOpen, setIsOpen }) => {
+  const navigate = useNavigate();
+  const menus = [
+    { name: "Dashboard", path: "/dashboard", icon: <MdDashboard /> },
+    { name: "Income", path: "/income", icon: <FaMoneyBillWave /> },
+    { name: "Expenses", path: "/expenses", icon: <FaWallet /> },
+    { name: "Categories", path: "/categories", icon: <BsStack /> },
+    { name: "Budgets", path: "/budget", icon: <FaChartBar /> },
+    { name: "Reports", path: "/reports", icon: <HiDocumentChartBar /> },
+    { name: "AI Advisor", path: "/ai-advisor", icon: <FaRobot /> },
+    { name: "Settings", path: "/settings", icon: <FaCog /> },
+  ];
   return (
-    <div>MobileMenu</div>
-  )
-}
+    <>
+      <div
+        className={`fixed inset-0 bg-black/40 z-40 transition ${isOpen ? "block" : "hidden"}`}
+        onClick={() => setIsOpen(false)}
+      >
+        <aside
+          className={`fixed top-0 left-0 w-65 h-screen bg-white z-50 p-5 overflow-y-auto
+          transition-all duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+        >
+          <div className="flex items-center justify-between mb-8">
+            <h1 className="text-2xl font-bold text-sky-800 ">IntelliSense</h1>
+            <button
+              onClick={() => setIsOpen(true)}
+              className="w-10 h-10 rounded-full border border-gray-100 lg:hidden items-center justify-center"
+            >
+              <FaTimes size={14} />
+            </button>
+          </div>
+          <div className="flex flex-col gap-2">
+            {menus.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.path}
+                className={({ isActive }) =>
+                  `flex items-center gap-3 px-4 py-3 rounded-xl text-sm font-medium transition-all ${isActive ? "bg-sky-800 text-white" : "text-gray-600 hover:bg-gray-100"}`
+                }
+              >
+                {item.icon}
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
+        </aside>
+      </div>
+    </>
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { MdDashboard } from "react-icons/md";
+import { BsStack } from "react-icons/bs";
+import { HiDocumentChartBar } from "react-icons/hi";
+import {
+  FaChartBar,
+  FaCog,
+  FaMoneyBillWave,
+  FaRobot,
+  FaWallet,
+} from "react-icons/fa";
+import { NavLink, useNavigate } from "react-router";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const navigate = useNavigate();
+  const menus = [
+    { name: "Dashboard", path: "/dashboard", icon: <MdDashboard /> },
+    { name: "Income", path: "/income", icon: <FaMoneyBillWave /> },
+    { name: "Expenses", path: "/expenses", icon: <FaWallet /> },
+    { name: "Categories", path: "/categories", icon: <BsStack /> },
+    { name: "Budgets", path: "/budget", icon: <FaChartBar /> },
+    { name: "Reports", path: "/reports", icon: <HiDocumentChartBar /> },
+    { name: "AI Advisor", path: "/ai-advisor", icon: <FaRobot /> },
+    { name: "Settings", path: "/settings", icon: <FaCog /> },
+  ];
 
-export default Sidebar
+  return (
+    <>
+      <aside
+        className="w-62.5 bg-white border border-gray-100 h-screen fixed top-0
+     px-5 py-6 hidden lg:block overflow-y-auto"
+      >
+        <h1 className="text-2xl font-bold text-sky-800 ">IntelliSense</h1>
+        <div className="flex flex-col gap-2">
+          {menus.map((item, index) => (
+            <NavLink
+              key={index}
+              to={item.path}
+              className={({ isActive }) =>
+                `flex items-center gap-3 px-4 py-3 rounded-xl text-sm font-medium transition-all ${isActive ? "bg-sky-800 text-white" : "text-gray-600 hover:bg-gray-100"}`
+              }
+            >
+              {item.icon}
+              {item.name}
+            </NavLink>
+          ))}
+        </div>
+        <div className="mt-12 bg-[#f8f7ff] border border-[#ddd8ff] rounded-2xl p-4">
+          <p className="text-sm font-semibold mb-2">Your Financial AI Assistant</p>
+          <p className="text-xs text-gray-500 mb-4">Get smart monthly insights and improve your savings habits.</p>
+          <button className="w-full bg-sky-800 text-white py-2 rounded-lg text-sm">Ask AI Advisor</button>
+        </div>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
